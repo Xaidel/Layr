4011d11f26f84dc8cb4f5c4bd82c0595694fe013
xaidel <abechuelak@gmail.com>
2025-10-05 05:31:40 +0800

    feat: applayout

--- client/src/components/ui/applayout.tsx
+++ client/src/components/ui/applayout.tsx
@@ -4,6 +4,13 @@ export default function AppLayout() {
   return (
     <>
       <AppHeader />
+      <div className="flex h-screen">
+        <aside className="h-full flex-1 border-r border-border ">
+        </aside>
+        <main className="h-full w-full flex-3 "></main>
+        <aside className="h-full w-full flex-1 border-l border-border">
+        </aside>
+      </div>
     </>
   )
 }
