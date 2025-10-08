fbdcf2973273c7e3d17e55f2cc9f67069b762f15
Shaina Pachica <shainapachica@gmail.com>
2025-10-08 17:15:09 +0800

    feat: made it scrollable, selectable and draggable. Also updated folder structure

---
+++ client/src/app/propspanel.tsx
@@ -0,0 +1,9 @@
+export default function PropsPanel() {
+  return (
+    <>
+      <aside className="h-full w-72 border-l border-border bg-background overflow-auto">
+
+      </aside>
+    </>
+  );
+}
